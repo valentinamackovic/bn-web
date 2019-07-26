411aae2944f4c345cbac2623188d7722631c8713
Jason <jayvdb1@gmail.com>
2019-07-26 10:41:54 +0200

    Added break point to adjust flex value so date doesn't wrap to new line

--- src/components/pages/admin/fans/FanActivityCardRow.js
+++ src/components/pages/admin/fans/FanActivityCardRow.js
@@ -3,36 +3,52 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core";
 import classNames from "classnames";
 
-const styles = theme => ({
-	root: {
-		display: "flex",
-		alignItems: "center"
-	},
-	shaded: {
-		backgroundColor: "#F5F7FA",
-		borderRadius: 8
-	},
-	heading: {
-		textTransform: "capitalize"
-	}
-});
+const styles = theme => {
+	return {
+		root: {
+			display: "flex",
+			alignItems: "center"
+		},
+		shaded: {
+			backgroundColor: "#F5F7FA",
+			borderRadius: 8
+		},
+		heading: {
+			textTransform: "capitalize"
+		},
+		col1: {
+			flex: 1,
+			textAlign: "center"
+		},
+		col2: {
+			flex: 3,
+			textAlign: "left",
+			[theme.breakpoints.only("lg")]: {
+				flex: 4
+			}
+		},
+		col4: {
+			flex: 14,
+			textAlign: "left"
+		},
+		col5: {
+			flex: 3,
+			textAlign: "right"
+		}
+	};
+};
 
 const FanActivityCardRow = props => {
 	const { heading, children, shaded, classes } = props;
 
-	const columnStyles = [
-		{ flex: 1, textAlign: "center" },
-		{ flex: 3, textAlign: "left" },
-		{ flex: 14, textAlign: "left" },
-		{ flex: 3, textAlign: "right" }
-	];
-
 	const columns = children.map((child, index) => {
 		return (
 			<span
-				className={classNames({ [classes.heading]: heading })}
+				className={classNames({
+					[classes.heading]: heading,
+					[classes[`col${index}`]]: true
+				})}
 				key={index}
-				style={columnStyles[index]}
 			>
 				{child}
 			</span>
