69573849e5503ef258ab21be2077b3d41768dfd8
shanimal <shanimal@tari.com>
2019-08-01 17:02:34 +0200

    Swapped redeemed by / for on fan check-ins

--- src/components/pages/admin/fans/FanHistoryActivityCard.js
+++ src/components/pages/admin/fans/FanHistoryActivityCard.js
@@ -373,7 +373,7 @@ class FanHistoryActivityCard extends Component {
 												[classes.boldSpan]: true
 											})}
 										>
-											{redeemed_by.full_name}&nbsp;
+											{redeemed_for.full_name}&nbsp;
 										</span>
 										<span className={classes.boldSpan}>checked-in&nbsp;</span>
 										<span>to {name} (1 Ticket)</span>
@@ -425,7 +425,7 @@ class FanHistoryActivityCard extends Component {
 											</Link>
 											) scanned by&nbsp;
 											<span className={classes.pinkSpan}>
-												{redeemed_for.full_name}&nbsp;
+												{redeemed_by.full_name}&nbsp;
 											</span>
 											on&nbsp;
 											<span className={classes.greySubtitle}>{occurredAt}</span>
@@ -1044,7 +1044,7 @@ class FanHistoryActivityCard extends Component {
 												[classes.boldSpan]: true
 											})}
 										>
-											{redeemed_by.full_name}&nbsp;
+											{redeemed_for.full_name}&nbsp;
 										</span>
 										<span className={classes.boldSpan}>checked-in&nbsp;</span>
 										to {name} (1 Ticket)
@@ -1100,7 +1100,7 @@ class FanHistoryActivityCard extends Component {
 											</Link>
 											) scanned by&nbsp;
 											<span className={classes.pinkSpan}>
-												{redeemed_for.full_name}&nbsp;
+												{redeemed_by.full_name}&nbsp;
 											</span>
 											on&nbsp;
 											<span className={classes.greySubtitle}>{occurredAt}</span>
