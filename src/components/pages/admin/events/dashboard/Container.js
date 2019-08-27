582feef66042c40f667454f414acebe3c2678dfb
Mike Berry <mikethetike@tari.com>
2019-08-27 10:45:40 +0200

    Add check for extra data

--- src/components/pages/admin/events/dashboard/Container.js
+++ src/components/pages/admin/events/dashboard/Container.js
@@ -663,18 +663,19 @@ class EventDashboardContainer extends Component {
 								</React.Fragment>
 							) : null}
 
-							{/*TODO add back when Mike wants to work on it*/}
-							{event.is_external ? null : (
-								<Typography className={classes.menuText}>
-									{this.renderMarketingMenu()}
-									<StyledLink
-										underlined={subheading === "marketing"}
-										onClick={this.handleMarketingMenu.bind(this)}
-									>
+							{!event.is_external &&
+							event.extra_data_admin &&
+							event.extra_data_admin.preview_facebook_enabled ? (
+									<Typography className={classes.menuText}>
+										{this.renderMarketingMenu()}
+										<StyledLink
+											underlined={subheading === "marketing"}
+											onClick={this.handleMarketingMenu.bind(this)}
+										>
 										Marketing
-									</StyledLink>
-								</Typography>
-							)}
+										</StyledLink>
+									</Typography>
+								) : null}
 
 							<Hidden smDown>
 								<div className={classes.additionalDesktopMenuContent}>
