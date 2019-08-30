ba8d7f3c840211e3f959df52cd786c056739e5d9
Jason <jayvdb1@gmail.com>
2019-08-30 08:46:37 +0200

    Using stateless component

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,18 +2,15 @@ import Routes from "./routes/Routes";
 import React from "react";
 import { Helmet } from "react-helmet";
 
-class Application extends React.Component {
-	render() {
-		return (
-			<div>
-				<Helmet>
-					{process.env.NODE_ENV !== "production" ? (
-						<meta name="robots" content="noindex"/>
-					) : null}
-				</Helmet>
-				<Routes/>
-			</div>
-		);
-	}
-}
+const Application = () => (
+	<React.Fragment>
+		<Helmet>
+			{process.env.NODE_ENV !== "production" ? (
+				<meta name="robots" content="noindex"/>
+			) : null}
+		</Helmet>
+		<Routes/>
+	</React.Fragment>
+);
+
 export default Application;
