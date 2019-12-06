1f02d8cff149b090b6636b0db960b958c6f81ee0
Shan <47271333+shanimal08@users.noreply.github.com>
2019-12-06 13:31:28 +0200

    Added breakpoint for button padding (#2048)

--- src/components/elements/Button.js
+++ src/components/elements/Button.js
@@ -48,7 +48,10 @@ const styles = theme => {
 			width: 134,
 			border: " 1px solid #979797",
 			borderRadius: 3,
-			padding: 10
+			padding: 10,
+			[theme.breakpoints.down("md")]: {
+				padding: 5
+			}
 		},
 		default: {
 			background: `linear-gradient(45deg, #FFF 10%, #FFF 90%)`,
@@ -155,7 +158,10 @@ const CustomButton = props => {
 	}
 
 	const useThinLabel =
-		variant === "additional" || variant === "text" || variant === "default" || variant === "plainWhite" ;
+		variant === "additional" ||
+		variant === "text" ||
+		variant === "default" ||
+		variant === "plainWhite";
 
 	return (
 		<Button
